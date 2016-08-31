2469cfc25c9887742f01b7529e148b0941279c4d
amit-upadhyay-IT <youarefreak1@gmail.com>
2016-08-31 12:02:43 +0530

    PostActivity added(Where user can post their activity)

--- SimpleBlog/app/src/main/java/com/amitupadhyay/simpleblog/PostActivity.java
+++ SimpleBlog/app/src/main/java/com/amitupadhyay/simpleblog/PostActivity.java
@@ -1,13 +1,45 @@
 package com.amitupadhyay.simpleblog;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class PostActivity extends AppCompatActivity {
+
+    private ImageButton mSelectImage;
+    private static final int GALLERY_REQUEST = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
+
+        mSelectImage = (ImageButton) findViewById(R.id.imageSelect);
+
+        mSelectImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                galleryIntent.setType("image/*");
+                startActivityForResult(galleryIntent, GALLERY_REQUEST);
+
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == GALLERY_REQUEST && resultCode == RESULT_OK)
+        {
+            Uri imageUri = data.getData();
+
+            mSelectImage.setImageURI(imageUri);
+        }
     }
 }
