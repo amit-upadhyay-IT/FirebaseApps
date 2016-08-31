737e53db0c92f33dbd37a00416669644c79ebf43
amit-upadhyay-IT <youarefreak1@gmail.com>
2016-08-31 10:17:04 +0530

    Simple Blog

---
+++ SimpleBlog/app/src/main/java/com/amitupadhyay/simpleblog/MainActivity.java
@@ -0,0 +1,22 @@
+package com.amitupadhyay.simpleblog;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+}
