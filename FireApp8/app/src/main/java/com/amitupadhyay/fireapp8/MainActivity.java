b8b6f631cb143b6e5ac355bab547f8fba6c5aa0c
amit-upadhyay-IT <youarefreak1@gmail.com>
2016-09-01 09:31:51 +0530

    FireApp9- capture image sends to server and displays from server

---
+++ FireApp8/app/src/main/java/com/amitupadhyay/fireapp8/MainActivity.java
@@ -0,0 +1,94 @@
+package com.amitupadhyay.fireapp8;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button mUploadBtn;
+    private ImageView mImageView;
+
+    private static final int CAMERA_REQUEST_CODE = 1;
+
+    private StorageReference mStorage;
+
+    private ProgressDialog mProgress;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mStorage = FirebaseStorage.getInstance().getReference();
+
+        mUploadBtn = (Button) findViewById(R.id.upload);
+        mImageView = (ImageView) findViewById(R.id.imageView);
+
+        mProgress = new ProgressDialog(this);
+
+        mUploadBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, CAMERA_REQUEST_CODE);
+
+
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == CAMERA_REQUEST_CODE && resultCode == RESULT_OK)
+        {
+
+            mProgress.setMessage("Uploading Image...");
+            mProgress.show();
+
+            Uri uri = data.getData();
+
+            StorageReference filepath = mStorage.child("photos").child(uri.getLastPathSegment());
+            filepath.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+                    mProgress.dismiss();
+
+                    Uri downloadUri = taskSnapshot.getDownloadUrl();
+
+                    Picasso.with(MainActivity.this).load(downloadUri).fit().centerCrop().into(mImageView);
+
+                    Toast.makeText(MainActivity.this, "Uploading finished ..", Toast.LENGTH_SHORT).show();
+
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+
+                    Toast.makeText(MainActivity.this, "Error", Toast.LENGTH_SHORT).show();
+
+                }
+            });
+        }
+
+    }
+}
